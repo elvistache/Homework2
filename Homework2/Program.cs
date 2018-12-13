7b1b5e4cbe67d9f15d56ca21efc317014a84b976
Tache Elvis <elvis.tache@outlook.com>
2018-12-13 12:57:16 +0200

    add birthdate and gender input

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework2
 {
+    
+    
     class Program
     {
+
+
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Enter your birthdate in the dd-mm-yyyy  format:");
+            string date = Console.ReadLine();
+
+            Console.WriteLine("Are you male or Female? (M/F)");
+            string gender = Console.ReadLine();
+
+            DateTime birthdate = DateTime.Parse(date);
+            TimeSpan age = DateTime.Now.Subtract(birthdate);
+
+            double yo = age.Days / 365;
+            Console.WriteLine("You are {0} years old.", yo);
+
+
+            Console.ReadLine();
+            Environment.Exit(0);
+        } 
     }
 }
