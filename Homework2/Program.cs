699d78f977ce3bcaad27fef29f288824e5975b39
Tache Elvis <elvis.tache@outlook.com>
2018-12-14 12:25:20 +0200

    update December 14

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -2,29 +2,65 @@
 
 namespace Homework2
 {
-    
-    
+
+
+    public enum Gender
+    {
+        male,
+        female
+    }
     class Program
     {
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your birthdate in the dd-mm-yyyy  format:");
+        BirthdateDefinition:
+            Console.WriteLine("Enter your birthdate in the mm-dd-yyyy  format:");
             string date = Console.ReadLine();
+            DateTime birthdate = DateTime.Parse(date);
 
+            
+
+        //TimeSpan age = DateTime.Now.Subtract(birthdate);
+
+        //double yo = age.Days / 365;
+        //Console.WriteLine("You are {0} years old.", yo);
+
+        GenderDefinition:
             Console.WriteLine("Are you male or Female? (M/F)");
             string gender = Console.ReadLine();
 
-            DateTime birthdate = DateTime.Parse(date);
-            TimeSpan age = DateTime.Now.Subtract(birthdate);
+            Gender person = new Gender();
 
-            double yo = age.Days / 365;
-            Console.WriteLine("You are {0} years old.", yo);
+            if (gender == "M")
+            {
+                person = Gender.male;
+                Console.WriteLine("you are a male");
+            }
+            else if (gender == "F")
+            {
+                person = Gender.female;
+            }
+            else
+            {
+                Console.WriteLine("invalid gender");
+                goto GenderDefinition;
+            }
+            
+            int numericValue = (int)person;
+            if (person == Gender.male)
+            {
+                Console.WriteLine("you rest at 65");
+            }
 
-
+            if (person == Gender.female)
+            {
+                Console.WriteLine("you rest at 63");
+            }
+           
             Console.ReadLine();
             Environment.Exit(0);
-        } 
+        }
     }
 }
