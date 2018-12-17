a532b36b19081ddd9351ba258d25db362675b367
Tache Elvis <elvis.tache@outlook.com>
2018-12-17 20:48:09 +0200

    final version

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -6,8 +6,8 @@ namespace Homework2
 
     public enum Gender
     {
-        male,
-        female
+        Male = 0,
+        Female = 0  
     }
     class Program
     {
@@ -15,50 +15,53 @@ namespace Homework2
 
         static void Main(string[] args)
         {
+
         BirthdateDefinition:
             Console.WriteLine("Enter your birthdate in the mm-dd-yyyy  format:");
             string date = Console.ReadLine();
             DateTime birthdate = DateTime.Parse(date);
 
-            
+        AgeCalculator:
+            TimeSpan age = DateTime.Now.Subtract(birthdate);
 
-        //TimeSpan age = DateTime.Now.Subtract(birthdate);
-
-        //double yo = age.Days / 365;
-        //Console.WriteLine("You are {0} years old.", yo);
+            int yo = age.Days / 365;
+            Console.WriteLine("You are {0} years old.", yo);
 
         GenderDefinition:
             Console.WriteLine("Are you male or Female? (M/F)");
-            string gender = Console.ReadLine();
-
-            Gender person = new Gender();
-
-            if (gender == "M")
+            string @char = Console.ReadLine();
+            if (@char == "m")
             {
-                person = Gender.male;
-                Console.WriteLine("you are a male");
+                Gender gender = Gender.Male;
+                TimeSpan time = DateTime.Now.AddYears(65 - yo) - DateTime.Now;
+                if (time.TotalDays / 365 < 0)
+                {
+                    Console.WriteLine("The person is retired");
+                }
+                else
+                {
+                    Console.WriteLine("The person will be retired in {0} years.", time.TotalDays / 365);
+                }
             }
-            else if (gender == "F")
+            else if (@char == "f")
             {
-                person = Gender.female;
+                Gender gender = Gender.Female;
+                TimeSpan time = DateTime.Now.AddYears(63 - yo) - DateTime.Now;
+                if (time.TotalDays / 363 < 0)
+                {
+                    Console.WriteLine("The person is retired");
+                }
+                else
+                {
+                    Console.WriteLine("The person will be retired in {0} years.", time.TotalDays / 365);
+                }
             }
             else
             {
-                Console.WriteLine("invalid gender");
+                Console.WriteLine("invalid gender. please enter it again");
                 goto GenderDefinition;
             }
             
-            int numericValue = (int)person;
-            if (person == Gender.male)
-            {
-                Console.WriteLine("you rest at 65");
-            }
-
-            if (person == Gender.female)
-            {
-                Console.WriteLine("you rest at 63");
-            }
-           
             Console.ReadLine();
             Environment.Exit(0);
         }
